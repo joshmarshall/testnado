4e315ac7efcaa523966f0a5131eb96b44bade0bf
Joshua Marshall <catchjosh@gmail.com>
2013-12-28 19:46:04 -0600

    Bumping.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="testnado",
-    version="0.1",
+    version="0.1.1",
     description="Tornado test helpers and Selenium testing with Tornado",
     author='Josh Marshall',
     author_email='catchjosh@gmail.com',
