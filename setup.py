8cbf434eb0cb7fc8c213db68a4fe3003510b118b
Joshua Marshall <catchjosh@gmail.com>
2013-12-28 18:14:32 -0600

    Adding requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="testnado",
+    version="0.1",
+    description="Tornado test helpers and Selenium testing with Tornado",
+    author='Josh Marshall',
+    author_email='catchjosh@gmail.com',
+    url="http://github.com/joshmarshall/testnado/",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    packages=["testnado"],
+    install_requires=["tornado", "selenium", "mock", "nose"]
+)
