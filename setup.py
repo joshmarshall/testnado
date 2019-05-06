74c9796cd90bd1e035c6e5c54b87b09ed3c056ac
Josh Marshall <catchjosh@gmail.com>
2019-05-06 13:45:30 +0900

    Tweaking setup.py for Travis, and only deploying once.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import sys
 from setuptools import setup, find_packages
 
@@ -7,7 +8,7 @@ from setuptools import setup, find_packages
 with open("VERSION") as version_fp:
     VERSION = version_fp.read().strip()
 
-
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 INSTALL_REQUIRES = ["tornado>=4"]
 
 if sys.version_info[0] == 2:
@@ -21,6 +22,6 @@ setup(
     author_email='catchjosh@gmail.com',
     url="http://github.com/joshmarshall/testnado/",
     license="http://www.apache.org/licenses/LICENSE-2.0",
-    packages=find_packages(exclude=["tests", "dist"]),
+    packages=find_packages(where=PROJECT_ROOT, exclude=["tests", "dist"]),
     install_requires=INSTALL_REQUIRES
 )
