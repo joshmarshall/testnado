21d951e553d88b1845d52809cc3e991cb75712a9
Akeem McLennon <amclennon@ustudio.com>
2020-08-21 10:55:39 -0500

    Fix typo in test assertion

--- tests/test_mock_service.py
+++ tests/test_mock_service.py
@@ -41,12 +41,12 @@ class TestMockService(AsyncTestCase, ServiceTestHelpers):
         response = yield self.fetch(
             self.service.url("/"), method="POST", body="FOOBAR")
         self.assertEqual(200, response.code)
-        self.assertEqual("PATCHED", response.body.decode("utf-8"))
+        self.assertEqual("RESPONSE", response.body.decode("utf-8"))
 
         response = yield self.fetch(
             self.service.url("/"), method="PATCH", body="FOOBAR")
         self.assertEqual(200, response.code)
-        self.assertEqual("RESPONSE", response.body.decode("utf-8"))
+        self.assertEqual("PATCHED", response.body.decode("utf-8"))
 
         response = yield self.fetch(self.service.url("/"), method="OPTIONS")
         self.assertEqual(405, response.code)
